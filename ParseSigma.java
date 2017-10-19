46988b8effae93891f3bef651bada54227613bd9
ZGLindo <Zaki.Lindo@gmail.com>
2017-10-19 07:36:20 -0400

    Initial commit. Created the java program needed to fulfill the task at hand. Included comments to show objectives in code.

---
+++ ParseSigma.java
@@ -0,0 +1,21 @@
+/*
+* Name: Zaki G. Lindo
+* Assignment: p001 - Splurth Elemental Symbols
+* Course: CIS 301 - Theory of Computation
+* Date: 09-22-2017
+*/
+
+import java.util.*
+
+// Program Description
+
+public class ParseSigma {
+
+	public static void main (String [] args) {
+		//Grab args. It is the filename a user inputs. 
+		//Check for the file
+
+		//Prompt user for a string
+		//Check the string to make sure it only contains letters of the alphabet
+	}
+}
